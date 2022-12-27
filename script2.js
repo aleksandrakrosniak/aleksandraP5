241b18b609d835c7c3a1df624837cd7b6010ff6c
PiotrKrosniak <p.krosniak@gmail.com>
2022-12-27 10:29:26 +0100

    added second picture

--- script2.js
+++ script2.js
@@ -1,21 +1,21 @@
-let img, 
-colorFG = '#111111',
-colorBG = '#f1f1f1';
+let img2, 
+colorFG2 = '#111111',
+colorBG2 = '#f1f1f1';
 
 function preload(){
-img = loadImage('https://piotrkrosniak.github.io/aleksandraP5/images/oplatwadodenim.jpg'); 			  			//add an image
+img2 = loadImage('https://piotrkrosniak.github.io/aleksandraP5/images/denimroots.jpg'); 			  			//add an image
 }
 
 function setup() {
-createCanvas(600, 600).parent('sketch-container');
+createCanvas(600, 600).parent('sketch-container2');
 background(colorBG);
-img.resize(600, 600);
+img2.resize(600, 600);
 }
 
 
 function draw() {
-background(colorBG);
-fill(colorFG);
+background(colorBG2);
+fill(colorFG2);
 noStroke();
 const ratio = 600/600;
 let coefZ = (mouseX/width)*400;
@@ -24,9 +24,9 @@ console.log(mouseX)
 const tilesX = map(coefZ, 0, 600, 10, 100);
 const tilesY = ratio * tilesX;
 const tileSize = width / tilesX;
-for (let y = 0; y < img.height; y += tileSize) {
-for (let x = 0; x < img.width; x += tileSize) {
-  let c = img.get(x, y);
+for (let y = 0; y < img2.height; y += tileSize) {
+for (let x = 0; x < img2.width; x += tileSize) {
+  let c = img2.get(x, y);
   let b = map(brightness(c), 0, 255, 1, 0);
   push();
   translate(x, y);
