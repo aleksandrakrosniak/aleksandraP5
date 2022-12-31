7094d56ad1bac5789fa6d7ac61734469258f5209
aleksandrakrosniak <a.krosniak@gmail.com>
2022-12-31 13:59:59 +0100

    400

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var sketch1 = function(p) {
     p.background(p.colorBG);
     p.fill(p.colorFG);
     p.noStroke();
-    p.ratio = 600 / 600;
+    p.ratio = 400 / 400;
     p.coefZ = 1900;//initial value of pixelization 
     if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY< p.height) 
     {p.coefZ = (p.mouseX / p.width) * 900;}
@@ -56,7 +56,7 @@ var sketch2 = function(p) {
     p.background(p.colorBG);
     p.fill(p.colorFG);
     p.noStroke();
-    p.ratio = 600 / 600;
+    p.ratio = 400 / 400;
     p.coefZ = 1900;
     if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY< p.height) 
     {p.coefZ = (p.mouseX / p.width) * 900;}
