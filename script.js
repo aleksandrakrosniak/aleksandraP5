fe8cbd2803659df16f052da1e4347c644452c799
aleksandrakrosniak <a.krosniak@gmail.com>
2022-12-31 14:24:16 +0100

    s

--- script.js
+++ script.js
@@ -95,10 +95,10 @@ var sketch3 = function(p) {
     p.fill(p.colorFG);
     p.noStroke();
     p.ratio = 600 / 600;
-    p.coefZ = 1500;//initial value of pixelization 
+    p.coefZ = 1900;//initial value of pixelization 
     if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY< p.height) 
     {p.coefZ = (p.mouseX / p.width) * 600;}
-    p.tilesX = p.map(p.coefZ, 0, 600, 1, 100);
+    p.tilesX = p.map(p.coefZ, 0, 900, 1, 100);
     p.tilesY = p.ratio * p.tilesX;
     p.tileSize = p.width / p.tilesX;
     for (p.y = 0; p.y < img.height; p.y += p.tileSize) {
